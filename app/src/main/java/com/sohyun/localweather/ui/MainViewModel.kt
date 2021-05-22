41f300ae898055289f09e218cc7a8d877023081f
thgus5614@naver.com <thgus5614@naver.com>
2021-05-22 22:57:16 +0900

    Change getWeather data login on MainViewModel.kt

--- app/src/main/java/com/sohyun/localweather/ui/MainViewModel.kt
+++ app/src/main/java/com/sohyun/localweather/ui/MainViewModel.kt
@@ -3,20 +3,20 @@ package com.sohyun.localweather.ui
 import androidx.lifecycle.*
 import com.sohyun.localweather.data.model.LocalWeather
 import com.sohyun.localweather.data.model.LocationResponse
+import com.sohyun.localweather.data.model.WeatherResponse
 import com.sohyun.localweather.data.network.NetworkStatus
 import com.sohyun.localweather.data.repository.WeatherRepository
 import com.sohyun.localweather.getToday
 import com.sohyun.localweather.getTomorrow
 import dagger.hilt.android.lifecycle.HiltViewModel
-import kotlinx.coroutines.async
-import kotlinx.coroutines.awaitAll
-import kotlinx.coroutines.launch
+import kotlinx.coroutines.*
 import javax.inject.Inject
 
 @HiltViewModel
 class MainViewModel @Inject constructor(
     private val weatherRepository: WeatherRepository,
 ) : ViewModel() {
+    val scope = CoroutineScope(Job() + Dispatchers.IO) // fixme
     private val isLoading = MutableLiveData(false)
     private val locations = MutableLiveData<List<LocationResponse>>(null)
     private val localWeather = MutableLiveData<List<LocalWeather>>().apply {
@@ -41,31 +41,46 @@ class MainViewModel @Inject constructor(
         val weatherItems = arrayListOf<LocalWeather>()
         viewModelScope.launch {
             locations.value?.let {
-                weatherItems.add(LocalWeather("Local", 0, null, null))
-                val todayDate = getToday() // FIXME
-                val tomorrowDate = getTomorrow() // FIXME
-                for(location in locations.value!!) {
-                    val todayWeatherDeferred = async { weatherRepository.getWeather(location.woeid, todayDate) }
-                    val tomorrowWeatherDeferred = async { weatherRepository.getWeather(location.woeid, tomorrowDate) }
-                    val response = awaitAll(todayWeatherDeferred, tomorrowWeatherDeferred)
-                    val todayWeather = when(response[0]){
-                        is NetworkStatus.Failure -> null
-                        is NetworkStatus.Success -> (response[0] as NetworkStatus.Success).data
+                weatherItems.add(LocalWeather("Local", 0, null, null)) // add title
+                val todayDate = getToday()
+                val tomorrowDate = getTomorrow()
+                val todayDeferred: List<Deferred<NetworkStatus<List<WeatherResponse.ConsolidatedWeather>>>> = locations.value!!.map {
+                    async {
+                        weatherRepository.getWeather(it.woeid, todayDate)
                     }
-                    val tomorrowWeather = when(response[1]){
-                        is NetworkStatus.Failure -> null
-                        is NetworkStatus.Success -> (response[1] as NetworkStatus.Success).data
-                    }
-                    weatherItems.add(LocalWeather(location.title, location.woeid, todayWeather?.get(0), tomorrowWeather?.get(0))) // FIXME
                 }
-                localWeather.postValue(weatherItems)
+                val tomorrowDeferred: List<Deferred<NetworkStatus<List<WeatherResponse.ConsolidatedWeather>>>> = locations.value!!.map {
+                    async {
+                        weatherRepository.getWeather(it.woeid, tomorrowDate)
+                    }
+                }
+                val todayResponse = todayDeferred.awaitAll()
+                val tomorrowResponse = tomorrowDeferred.awaitAll()
+                locations.value!!.forEachIndexed { index, location ->
+                    val todayWeather = when(todayResponse[index]){
+                        is NetworkStatus.Failure -> null
+                        is NetworkStatus.Success -> (todayResponse[index] as NetworkStatus.Success).data
+                    }
+                    val tomorrowWeather = when(tomorrowResponse[index]){
+                        is NetworkStatus.Failure -> null
+                        is NetworkStatus.Success -> (tomorrowResponse[index] as NetworkStatus.Success).data
+                    }
+                    weatherItems.add(LocalWeather(location.title, location.woeid, todayWeather?.get(0), tomorrowWeather?.get(0)))
+                }
+                setLocalWeather(weatherItems)
             }
             isLoading.value = false
         }
     }
 
+
     private val locationsObserve = Observer<List<LocationResponse>> {
         it?.let { searchWeather() }
+    }
+
+    fun setLocalWeather(weatherItems: List<LocalWeather>) {
+        localWeather.postValue(null)
+        localWeather.postValue(weatherItems)
     }
 
     fun isLoading(): LiveData<Boolean> = isLoading
@@ -78,6 +93,7 @@ class MainViewModel @Inject constructor(
     }
 
     override fun onCleared() {
+        scope.cancel()
         locations.removeObserver(locationsObserve)
         super.onCleared()
     }
